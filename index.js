3bbd05f2c40ca7f708340aa56fdcbe5c48510a9e
Illhm <gusmif@ilhamxander.web.id>
2026-04-02 15:33:22 +0700

    Convert entrypoint to ESM for Deno runtime

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { spawn } = require("child_process");
+import { spawn } from "node:child_process";
+import process from "node:process";
+import { fileURLToPath } from "node:url";
 
 const port = process.env.PORT || "3000";
 const host = process.env.HOST || "0.0.0.0";
-const nextBin = require.resolve("next/dist/bin/next");
+
+const nextBin = fileURLToPath(new URL("./node_modules/next/dist/bin/next", import.meta.url));
 
 const child = spawn(process.execPath, [nextBin, "start", "-p", port, "-H", host], {
   stdio: "inherit",
