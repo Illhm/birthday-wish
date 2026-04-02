3b394cf1bafcace5d0c253b439fe13c21e55b250
Illhm <gusmif@ilhamxander.web.id>
2026-04-02 14:50:40 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  images: {
-    loader: 'imgix',
-    path: '',
-    unoptimized: true,
-  },
-  // Tambahkan ini jika build masih rewel soal webpack
-  webpack5: true, 
 }
