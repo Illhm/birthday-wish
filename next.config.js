898f25f68460c785f0f7730d272047709f8ddc91
Illhm <gusmif@ilhamxander.web.id>
2026-04-02 14:48:34 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  // Penting: Deno Deploy static nggak bisa optimasi image otomatis lewat server
   images: {
     loader: 'imgix',
     path: '',
     unoptimized: true,
   },
-  // Memaksa pakai Webpack 5 (bawaan Next 11)
-  webpack5: true,
+  // Tambahkan ini jika build masih rewel soal webpack
+  webpack5: true, 
 }
